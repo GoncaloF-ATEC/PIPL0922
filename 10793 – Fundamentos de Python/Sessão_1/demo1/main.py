0792ebe3860678b046daad52893e904f4d38c8a2
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-10-04 09:41:57 +0100

    pos pass

---
+++ 10793 – Fundamentos de Python/Sessão_1/demo1/main.py
@@ -0,0 +1,61 @@
+#valor inteiro
+i = 10
+
+#valor texto
+txt = "Valor"
+
+#valor decimal
+d = 10.5
+
+#valor bool (T ou F)
+b = True
+
+#int + int
+soma = i + 23
+print(soma)
+
+""" 
+#int + str - Erro
+soma2 = i + " nome"
+print(soma2)
+"""
+
+
+#decimal + int
+soma3 = d + 23
+print(soma3)
+
+
+#bool(T) + int
+soma4 = b + 23
+print(soma4)
+
+#bool(F) + int
+b = False
+soma5 = b + 23
+print(soma5)
+
+
+inData = int(input("valor 1: "))
+inData2 = int(input("valor 2: "))
+
+print("inData", inData)
+print(f"inData2 {inData2}")
+
+soma6 = inData + inData2
+
+print(f"soma {soma6}")
+
+
+if soma6 == 10:
+    print("soma = 10")
+else:
+    print("outro valor")
+    print("outro valor 2")
+
+print("fora do else")
+
+if soma6 % 2 == 0:
+    pass
+
+print("pos empty block")
