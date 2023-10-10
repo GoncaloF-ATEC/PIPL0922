d4ff3430170ffccdca4c8a8360f1167fde5fca89
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-10-10 09:55:41 +0100

    loop - inicio

---
+++ 10793 – Fundamentos de Python/sessao_2/loops/main.py
@@ -0,0 +1,10 @@
+print("**************** 0 a 9 *************")
+
+for i in range(10):  # C ->  for(int i = 0; i > 10; i++){}
+    print(i) # print(i, end="\n")
+
+
+print("**************** 5 a 9 *************")
+
+for i in range(5, 10):  # C ->  for(int i = 0; i > 10; i++){}
+    print(i, end=" ")
