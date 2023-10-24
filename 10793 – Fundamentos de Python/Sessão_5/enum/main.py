1e66f3e47e73d3d725be2a63f80042252e8b279f
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-10-24 08:31:51 +0100

    Enums

---
+++ 10793 – Fundamentos de Python/Sessão_5/enum/main.py
@@ -0,0 +1,10 @@
+from utils import Cor
+
+
+
+c = Cor.Verde
+
+print(c)
+print(c.name)
+print(c.value)
+
---
+++ 10793 – Fundamentos de Python/Sessão_5/enum/utils.py
@@ -0,0 +1,8 @@
+from  enum import  Enum
+
+
+class Cor(Enum):
+    Vermelho = "Red"
+    Verde = "Green"
+    Azul = "Blue"
+
