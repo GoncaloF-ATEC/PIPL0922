7e2455afdae9068a46ad42e0e898024f4c0c821d
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-10-24 09:18:21 +0100

    metodos __,  overriding

--- 10793 – Fundamentos de Python/Sessão_5/Heranca/classes.py
+++ 10793 – Fundamentos de Python/Sessão_5/Heranca/classes.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-class classif(Enum):
+class Class(Enum):
     Anfibios = "Anfíbios"
     Aves = "Aves"
     Mamiferos = "Mamíferos"
@@ -11,9 +11,9 @@ class classif(Enum):
 class Animal:
     def __init__(self,
                  num_patas: int,
-                 nome: str ,
+                 nome: str,
                  cor: str,
-                 classsificacao: classif):
+                 classsificacao: Class):
         self.nome = nome
         self.num_patas = num_patas
         self.cor = cor
@@ -23,24 +23,53 @@ class Animal:
     def comer(self):
         print("o animal comeu")
 
+    def beber(self, liquido):
+        print(f"o {self.__class__.__name__} chamado {self.nome} bebeu {liquido}")
 
-class gato(Animal):
+    def emitir_som(self):
+        print("som")
+
+    def __str__(self):
+
+        msg = f"""
+        tipo: {self.__class__.__name__}
+        nome: {self.nome}
+        num_patas: {self.num_patas}
+        cor: {self.cor}
+        classsificacao: {self.classsificacao.value}
+        idade: {self.idade}
+        """
+        return msg
+
+
+    def __eq__(self, other):
+        return self.nome == other.nome
+
+
+    def __add__(self, other):
+        return Animal(4,"sem nome", "azlul", Class.Mamiferos)
+
+
+
+class Gato(Animal):
     def __init__(self,
                  num_patas: int,
                  nome: str,
                  cor: str,
-                 classsificacao: classif,
+                 classsificacao: Class,
                  num_vidas: int):
         self.num_vidas = num_vidas
         super().__init__(num_patas, nome, cor, classsificacao)
 
+    def emitir_som(self):
+        print("Miar")
 
-class piriquito(Animal):
+class Piriquito(Animal):
     def __init__(self,
                  num_patas: int,
                  nome: str,
                  cor: str,
-                 classsificacao: classif,
+                 classsificacao: Class,
                  num_asas: int):
         self.num_asas = num_asas
 
@@ -48,6 +77,10 @@ class piriquito(Animal):
         self.classsificacao = classsificacao
         self.num_patas = num_patas
         self.nome = nome
+        self.idade = 0
 
     def voar(self):
         print("a Voar")
+
+    def emitir_som(self):
+        print("piar")
--- 10793 – Fundamentos de Python/Sessão_5/Heranca/main.py
+++ 10793 – Fundamentos de Python/Sessão_5/Heranca/main.py
@@ -1,1 +1,20 @@
+from classes import *
 
+gato1 = Gato(4, "ze", "preto", Class.Mamiferos, 7)
+gato2 = Gato(4, "Maria", "amarelo", Class.Aves, 9)
+
+print(gato1)
+
+print(gato1 == gato2)
+
+
+print(gato1 + gato2)
+
+
+pi = Piriquito(4, "ze", "preto", Class.Mamiferos, 7)
+
+print(pi)
+
+print(type(pi))
+print(type(pi).__name__)
+print(pi.__class__.__name__)
